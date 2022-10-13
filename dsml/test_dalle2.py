d362f004a1117dff9ae3419bdea1e40c1458cae3
tulliopizzuti <tullio.pizz@gmail.com>
2022-10-13 15:29:20 +0200

    fix separatore parole + script dall-e2 web

---
+++ dsml/test_dalle2.py
@@ -0,0 +1,4 @@
+from dalle2 import Dalle2
+dalle = Dalle2("sess-m5P8qcas7Sv7A5vq2OiAvw9iJmmpW99IdMv5q0kB") 
+generations = dalle.generate("portal to another dimension, digital art")
+print(generations)
