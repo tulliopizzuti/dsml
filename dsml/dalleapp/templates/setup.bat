c27b534e343dc06ef51d318450df8509baabba4c
Domenico Pizzo <domenico.pizzo96@gmail.com>
2022-10-14 18:48:38 +0200

    Add setup.bat and setup.sh

---
+++ dsml/dalleapp/templates/setup.bat
@@ -0,0 +1,8 @@
+conda create --name dalleapp
+conda activate dalleapp
+
+conda install -c anaconda django
+conda install -c conda-forge nltk
+conda install -c conda-forge djangorestframework
+conda install -c anaconda transformers
+pip install dalle2
