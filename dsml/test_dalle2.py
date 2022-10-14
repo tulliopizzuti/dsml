949520eb64f8b11b568730d933c40d42c919a52c
tulliopizzuti <tullio.pizz@gmail.com>
2022-10-14 17:39:19 +0200

    generate multiple image

--- dsml/test_dalle2.py
+++ dsml/test_dalle2.py
@@ -1,4 +1,4 @@
 from dalle2 import Dalle2
 dalle = Dalle2("sess-m5P8qcas7Sv7A5vq2OiAvw9iJmmpW99IdMv5q0kB") 
-generations = dalle.generate("portal to another dimension, digital art")
+generations = dalle.generate_amount("portal to another dimension, digital art",4)
 print(generations)
