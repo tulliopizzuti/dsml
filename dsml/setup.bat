0458ca85962d9deb26a56bddff3c947971159385
Domenico Pizzo <domenico.pizzo96@gmail.com>
2022-10-15 19:45:56 +0200

    Add Demo Dalle App and edit setup.bat, setup.sh

--- dsml/setup.bat
+++ dsml/setup.bat
@@ -5,4 +5,5 @@ conda install -c anaconda django
 conda install -c conda-forge nltk
 conda install -c conda-forge djangorestframework
 conda install -c anaconda transformers
+conda install punkt
 pip install dalle2
--- dsml/setup.sh
+++ dsml/setup.sh
@@ -4,4 +4,5 @@ conda install -c anaconda django
 conda install -c conda-forge nltk
 conda install -c conda-forge djangorestframework
 conda install -c anaconda transformers
+conda install -c conda-forge nltk_data
 pip install dalle2
